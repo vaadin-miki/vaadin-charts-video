42bf8b4897ab66e3235e46a702b908ef8c62bc6e
Miki <miki@vaadin.com>
2017-05-24 09:01:28 +0300

    now Vaadin 8/Charts 4 compatible

--- intro-video/src/main/java/org/vaadin/miki/charts/intro/IntroChartsUI.java
+++ intro-video/src/main/java/org/vaadin/miki/charts/intro/IntroChartsUI.java
@@ -1,29 +1,23 @@
 package org.vaadin.miki.charts.intro;
 
-import java.util.Collection;
-
-import javax.servlet.annotation.WebServlet;
-
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.Configuration;
+import com.vaadin.addon.charts.model.DataProviderSeries;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.provider.ListDataProvider;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import org.vaadin.miki.charts.data.mock.MockDataService;
 import org.vaadin.miki.charts.data.model.Book;
 import org.vaadin.miki.charts.data.model.BookPrice;
 
-import com.vaadin.addon.charts.Chart;
-import com.vaadin.addon.charts.model.ChartType;
-import com.vaadin.addon.charts.model.Configuration;
-import com.vaadin.addon.charts.model.ContainerDataSeries;
-import com.vaadin.addon.charts.model.ListSeries;
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.annotations.Widgetset;
-import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import javax.servlet.annotation.WebServlet;
+import java.util.Collection;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -51,10 +45,13 @@ public class IntroChartsUI extends UI {
         
         for(Book book: books) {
         	Collection<BookPrice> prices = this.service.getPrices(book, 2011, 2012, 2013, 2014, 2015);
-        	BeanItemContainer<BookPrice> container = new BeanItemContainer<>(BookPrice.class, prices);
-        	ContainerDataSeries series = new ContainerDataSeries(container);
-        	series.setYPropertyId("amount");
-        	series.setXPropertyId("year");
+
+            // Vaadin 8 no longer has containers - instead we are using Java 8 awesomeness
+            ListDataProvider<BookPrice> provider = new ListDataProvider<>(prices);
+
+        	DataProviderSeries<BookPrice> series = new DataProviderSeries<>(provider);
+        	series.setY(BookPrice::getAmount);
+        	series.setX(BookPrice::getYear);
         	series.setName(book.toString());
         	config.addSeries(series);
         }
